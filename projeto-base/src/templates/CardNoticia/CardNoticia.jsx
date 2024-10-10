1a430f89adeb6988675a1bfd4856d83707d7d2c4
Gustavo Oliveira <ctt.gustavooliv@gmail.com>
2024-10-10 07:55:02 -0300

    Card com estilo certo

--- projeto-base/src/templates/CardNoticia/CardNoticia.jsx
+++ projeto-base/src/templates/CardNoticia/CardNoticia.jsx
@@ -4,21 +4,18 @@ import foto from '../../assets/images/Tecnologia1.png'
 function Card(props) {
 
     return (
-        <div className="card my-2 shadow-lg">
-            <h5 className="card-header">{props.manchete}</h5>
-            <div className="card-body">
-                <div className='text-center my-2'>
-                    <img className="card-title rounded-3 shadow" 
-                         src={props.foto ? 'data:image/jpeg;base64,' + props.foto : foto} alt="foto manchete" />
+        <div className="card">
+            <img className="img-card" 
+                src={props.foto ? 'data:image/jpeg;base64,' + props.foto : foto} alt="foto manchete" />
+            
+            <div className='conteudo'>
+            <h5 className="title-card">{props.manchete}</h5>
+                    <p className="text-card">{props.conteudo}</p>
                 </div>
-                <div>
-                    <p className="card-text">{props.conteudo}</p>
-                </div>
-                <div className='d-flex justify-content-end mt-2'>
+                <div className='d-flex justify-content-end mt-2 btn-ler'>
                     <button type='button' className="btn btn-primary btn-sm">Ler</button>
                 </div>
             </div>
-        </div>
     );
 }
 export default Card
